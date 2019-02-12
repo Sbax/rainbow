afd1806b9c0ca97d5f73973b1ac62e1798fb06c9
Matteo Bacci <matteo.bacci@antreem.com>
2019-02-12 12:57:04 +0100

    First actual commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const colors = [
+    "pink",
+    "purple",
+    "blue",
+    "azure",
+    "green",
+    "yellow",
+    "orange",
+    "red"
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    rainbowThis(value) {
+        if (!value) {
+            return value;
+        }
+
+        let colorIndex = 0;
+        const result = value.split("").map((letter, index) => {
+            if (letter.trim() === "")
+                return <span key={`letter-${index}`}>{letter}</span>;
+            colorIndex += 1;
+            return (
+                <span
+                    key={`letter-${index}`}
+                    className={colors[colorIndex % 8]}
+                >
+                    {letter}
+                </span>
+            );
+        });
+
+        return result;
+    }
+
+    state = {
+        boring: "",
+        rainbowed: this.rainbowThis("Copy here!")
+    };
+
+    eventToRainbow({ target }) {
+        if (!target) return;
+        const result = this.rainbowThis(target.value);
+
+        return this.setState({
+            boring: target.value,
+            rainbowed: result
+        });
+    }
+
+    render() {
+        return (
+            <main>
+                <input
+                    type="text"
+                    onChange={e => this.eventToRainbow(e)}
+                    value={this.state.boring}
+                    placeholder="Write here"
+                />
+
+                <div className="result">{this.state.rainbowed}</div>
+            </main>
+        );
+    }
 }
 
 export default App;
